36e354293adfc0a7ed94c9a6522e3b7756961b4c
Fabio Torchetti <mrwho@wedjaa.net>
2016-11-10 11:30:12 -0600

    New build.

--- build/serviceworker.js
+++ build/serviceworker.js
@@ -2,9 +2,10 @@
 
 importScripts('serviceworker-cache-polyfill.js');
 
-var CACHE_NAME = 'react-boilerplate-cache-v1';
+var CACHE_NAME = 'fit-calendar-cache-v1';
+
 // The files we want to cache
-var urlsToCache = ['/', '/css/main.css', '/js/bundle.js'];
+var urlsToCache = ['.', 'build/css/main.css', 'build/js/bundle.js'];
 
 // Set the callback for the install step
 self.addEventListener('install', function (event) {
